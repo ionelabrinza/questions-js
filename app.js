8285b15a3986e8728084dda99acb0dede95d1321
ionelabrinza <ionela.brinza@yahoo.com>
2022-08-29 10:41:15 -0500

    adding functionality to the buttons

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-//using selectors inside the element
 // traversing the dom
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const question = e.currentTarget.parentElement.parentElement;
+
+        question.classList.toggle("show-text");
+    });
+});
